224ea118d41eeeaf8b62c2eacb0915340ba2fff9
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-10-20 15:07:09 +0200

    Implemented second step of reverse algorithm which is reversing characters array

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getReversedString(somestring: string): string {
-    let splittedString = somestring.split(""); // 'hello' -> ["h", "e", "l", "l", "o"]
-    
+    let splittedString = somestring.split(''); // 'str' -> ['s', 't', 'r']
+    let reversedCharactersArray = splittedString.reverse(); // ['s', 't', 'r'] -> ['r', 't', 's']
+
     return somestring;
   }
 }
