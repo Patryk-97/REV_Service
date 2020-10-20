e198559b4a3bd20a4787849f5454434ae7d18f75
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-10-20 15:03:59 +0200

    Implemented first step of reverse algorithm which is splitting string into characters

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getReversedString(somestring: string): string {
+    let splittedString = somestring.split(""); // 'hello' -> ["h", "e", "l", "l", "o"]
+    
     return somestring;
   }
 }
