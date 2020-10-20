c97622b8fdba8566c4dbef3c833e8d9f6e4ae94f
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-10-20 15:29:05 +0200

    Added parameter to get method which is string to reverse so now entering in browser localhost:3000/someString causes display reversedString in browser

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { Query } from '@nestjs/common/decorators/http/route-params.decorator';
+import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getReversedString(): string {
-    return this.appService.getReversedString('testowyString');
+  @Get('/:stringToReverse')
+  getReversedString(@Param('stringToReverse') stringToReverse: string): string {
+    return this.appService.getReversedString(stringToReverse);
   }
 }
