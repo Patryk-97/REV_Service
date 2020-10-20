4ada8e450432c731a061ace3bf1ea00b6f43471d
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-10-20 15:02:23 +0200

    Changed function which will be invoked after enter phrase (localhost:3000) in browser to getReversedString and created empty getReversedString function in AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Query } from '@nestjs/common/decorators/http/route-params.decorator';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getReversedString(): string {
+    return this.appService.getHello('testowyString');
   }
 }
